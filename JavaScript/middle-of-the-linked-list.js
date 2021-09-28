e58f7ac1a14a23bd5ddfac6676f385ae85c7e7f4
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-09-28 16:27:00 +0530

    Create middle-of-the-linked-list.js
    Create middle-of-the-linked-list.js

---
+++ JavaScript/middle-of-the-linked-list.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let A = [head];
+    while (A[A.length - 1].next != null)
+        A.push(A[A.length - 1].next);
+    return A[Math.trunc(A.length / 2)];
+};
