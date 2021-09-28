a204b6a2098fd344ca93cf4270a45c7379a900cd
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-09-28 17:09:34 +0530

    Create climbing-stairs.js
    Create climbing-stairs.js

---
+++ JavaScript/climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var climbStairs = function(n) {
+    
+  if (n < 3) return n;
+  let first = 1;
+  let second = 2;
+  for (let i = 2; i < n; i++) {
+    const current = first + second;
+    first = second;
+    second = current;
+  }
+
+  return second;
+
+    
+};
