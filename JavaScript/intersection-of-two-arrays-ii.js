2925a623e70c20ca53d646965e8a2ffa00ddbfa2
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-09-28 16:40:19 +0530

    Create intersection-of-two-arrays-ii.js
    Create intersection-of-two-arrays-ii.js

---
+++ JavaScript/intersection-of-two-arrays-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+var intersect = function(nums1, nums2) {
+
+  let result = [];
+    
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums2.includes(nums1[i])) {
+      result.push(nums1[i]);
+      nums2.splice(nums2.indexOf(nums1[i]), 1);
+    }
+  }
+    
+  return result;
+};
