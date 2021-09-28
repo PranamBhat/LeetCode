2a668ddca9af7775127161c5c480f3c8df4e0416
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-09-28 17:16:34 +0530

    Create last-substring-in-lexicographical-order.js
    Create last-substring-in-lexicographical-order.js

---
+++ JavaScript/last-substring-in-lexicographical-order.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+const lastSubstring = (s) => {
+  let i = 0 // index of final substring.
+  let j = 1 // index of lookahead possible substring.
+  let k = 0 // moving offset to compare i & j.
+
+  while (j + k < s.length) {
+    if (s[i + k] < s[j + k]) {
+      i = Math.max(i + k + 1, j)
+      j = i + 1
+      k = 0
+    }
+
+    if (s[i + k] === s[j + k]) {
+      k += 1
+    }
+
+    if (s[i + k] > s[j + k]) {
+      j = j + k + 1
+      k = 0
+    }
+  }
+
+  return s.substring(i)
+}
+
+module.exports = lastSubstring
