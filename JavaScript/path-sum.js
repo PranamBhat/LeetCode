57887f8a636b2bac73d6fe7075658a97494f3c09
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-10-01 10:55:11 +0530

    Create path-sum.js
    Create path-sum.js

---
+++ JavaScript/path-sum.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+var hasPathSum = function(root, sum) {
+  if (!root) return false;
+  if (root.val === sum && !root.left && !root.right) return true;
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+};
