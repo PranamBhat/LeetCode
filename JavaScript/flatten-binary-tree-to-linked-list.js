6061b2ba658b1b750bbe2b57f8b48eb2f1b1b33d
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-10-13 09:31:07 +0530

    Create flatten-binary-tree-to-linked-list.js
    Create flatten-binary-tree-to-linked-list.js

---
+++ JavaScript/flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+
+var flatten = function(root) {
+    let curr = root
+    while (curr) {
+        if (curr.left) {
+            let runner = curr.left
+            while (runner.right) runner = runner.right
+            runner.right = curr.right, curr.right = curr.left, curr.left = null
+        }
+        curr = curr.right
+    }
+};
