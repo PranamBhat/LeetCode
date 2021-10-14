e628036e4854130103a41edff692b6d7413596e7
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-10-14 16:04:20 +0530

    Create search-a-2d-matrix-ii.js
    Create search-a-2d-matrix-ii.js

---
+++ JavaScript/search-a-2d-matrix-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+
+var searchMatrix = function(M, T) {
+    let y = M.length, i = 0, j = M[0].length - 1
+    while (i < y && ~j) {
+        let cell = M[i][j]
+        if (cell === T) return true
+        else if (cell > T) j--
+        else i++
+    }
+    return false
+};
