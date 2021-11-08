a9040b9298649807cfd98a9c4eeb9b32d4330fd7
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-11-08 09:45:24 +0530

    Create subarray-product-less-than-ksubmissions.js
    reate subarray-product-less-than-ksubmissions.js

---
+++ JavaScript/subarray-product-less-than-ksubmissions.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+var numSubarrayProductLessThanK = function(nums, k) {
+    
+    if (k <= 1) 
+        return 0;
+    var prod = 1;
+    var count = 0, left = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+            prod *= nums[i];
+    
+        while (prod >= k) prod /= nums[left++];
+            count += i - left + 1;
+        }
+    
+        return count;
+
+};
