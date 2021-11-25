1e68e802848383ad4b96b1f81c8470cea2005d50
Pranam K <32120033+PranamBhat@users.noreply.github.com>
2021-11-25 13:49:59 +0530

    Create k-closest-points-to-origin.js
    Create k-closest-points-to-origin.js

---
+++ JavaScript/k-closest-points-to-origin.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+
+var kClosest = function(points, k) {
+
+    points.sort((a,b)=> { return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0]+b[1]*b[1]) });
+    
+    return points.slice(0,k);
+    
+};
